c7607a63ac06cb790ad332251c23b4920436faea
Samarth Patel <samarth.patel.22@hotmail.com>
2019-08-30 18:55:40 -0400

    Created app framework

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Home Monitor</header>
+    </div>
+  );
+}
+
+export default App;
